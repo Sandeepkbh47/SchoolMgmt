c7509c84f9354cd91deb999eebea82561fbebbd2
Sandeep <sandeepkbh47@gmail.com>
2023-12-03 19:57:03 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,HomeComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
